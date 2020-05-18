0b5ee23838a7cbf9262b8fd3e016eadd0f4831d7
xcixor <pndungu54@gmail.com>
2020-05-18 06:10:56 +0300

    feat(orders): export order info as csv file

--- src/core/orders/admin.py
+++ src/core/orders/admin.py
@@ -1,3 +1,8 @@
+"""Admin page for orders
+"""
+import csv
+import datetime
+from django.http import HttpResponse
 from django.contrib import admin
 from .models import Order, OrderItem
 
@@ -7,8 +12,47 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def export_to_csv(modeladmin, request, queryset):
+    """Download order list as csv
+
+    Arguments:
+        modeladmin {object} -- Instance of the current model admin
+        being displayed
+        request {object} -- The current request object as an
+        HttpRequest instance
+        queryset {[type]} -- A QuerySet for the objects selected by the user
+
+    Returns:
+        [type] -- [description]
+    """
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;'\
+        'filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many
+              and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    """Manages orders.
+    """
     list_display = [
         'id', 'first_name', 'last_name', 'email',
         'address', 'postal_code', 'city', 'paid',
@@ -16,3 +60,5 @@ class OrderAdmin(admin.ModelAdmin):
         ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    extra = 1
+    actions = [export_to_csv]
